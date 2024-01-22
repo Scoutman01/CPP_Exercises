68a10730985418d7f2df8eeb494d384414760ba8
MOKEDDEM Sami <sami.mokeddem@edu.univ-eiffel.fr>
2024-01-22 14:12:15 +0100

    tp1 ex1 - q6

---
+++ tp1/ex1/main2.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+
+int main()
+{
+    std::cout << "Entre ton nom: ";
+
+    std::string name;
+    std::cin >> name;
+    std::cout << name << std::endl;
+
+    return 0;
+}
