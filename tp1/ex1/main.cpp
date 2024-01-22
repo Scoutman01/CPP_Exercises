1bc03cd4883e05460c0998a452bb9600a48e5223
MOKEDDEM Sami <sami.mokeddem@edu.univ-eiffel.fr>
2024-01-22 14:07:37 +0100

    tp1 - ex1 - q4

--- tp1/ex1/main.cpp
+++ tp1/ex1/main.cpp
@@ -3,11 +3,11 @@
 
 int main()
 {
-    std::cout << "Entre ton nom: ";
+    std::cout << "Entre ton nom: "; // affichage sur la sortie standard
 
-    char name[20] = "";
-    std::cin >> std::setw(20) >> name;
-    std::cout << "Bonjour Palluche La Falluche !" << std::endl;
+    char name[20] = ""; // création d'un tableau de caractères de taille 20
+    std::cin >> std::setw(20) >> name; // récupération de 19 caractères de l'entrée standard + un caractère nulle dans le tableau name
+    std::cout << name << std::endl; // affichage de la chaine dans la sortie standard
 
     return 0;
 }
